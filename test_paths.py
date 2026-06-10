6ee9733ca88eb443ce54ed2b1b9bd23edfde84df
kosmosofmalick-spec <kosmosofmalick@gmail.com>
2026-06-10 03:18:50 +0200

    Update test_paths.py

--- test_paths.py
+++ test_paths.py
@@ -1,13 +1,1 @@
-from paths import *
 
-print("R project:")
-print(R_PROJECT)
-
-print("\nOutput folder:")
-print(R_OUTPUT)
-
-print("\nIntermediate folder:")
-print(R_INTERMEDIATE)
-
-print("\nPython figures folder:")
-print(PYTHON_FIGURES)
